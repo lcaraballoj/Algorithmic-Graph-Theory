f003b8ac3cd215d981cef4d49de0191c321ab4dc
Linnea <ljc42000@gmail.com>
2023-12-20 17:59:57 -0500

    Working on converting database data to new format

---
+++ Algorithms/getDatabaseData.py
@@ -0,0 +1,50 @@
+# # --------
+# # Get data from a hypergraph database to convert into new form
+# # --------
+
+import os
+import pandas as pd
+import mysql.connector, json
+
+
+# Establish a connection to your MySQL database
+# Database connection information
+DB_USERNAME = 'root'
+DB_PASSWORD = 'password'
+DB_HOST = 'localhost'
+DB_NAME = 'hypergraphs'
+
+# Connect to the MySQL database
+connection = mysql.connector.connect(
+    host=DB_HOST,
+    user=DB_USERNAME,
+    passwd=DB_PASSWORD,
+    database=DB_NAME
+)
+
+# Create a cursor object to execute queries
+cursor = connection.cursor()
+
+# Example query to select data
+query = "SELECT * FROM kNEO INNER JOIN hypergraph USING (hypergraph) WHERE kNEO.two IS NOT NULL"
+
+# Execute the query
+cursor.execute(query)
+
+# Fetch all the rows
+rows = cursor.fetchall()
+
+list_of_dicts = [json.loads(row[0]).values() for row in rows] # Get the hyperedge vertex data
+
+sorted_data = [sorted(sublist, key=len, reverse=True) for sublist in list_of_dicts] # Sort hyperedges by size
+
+print(sorted_data)
+
+df = pd.read_sql(query,connection)
+
+print(df['two'].to_list())
+
+# Close the cursor and connection
+cursor.close()
+connection.close()
+
