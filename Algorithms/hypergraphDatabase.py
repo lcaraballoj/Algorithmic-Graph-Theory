d46b592428bd9008eba60968dbed5a0b93450aa0
Linnea <ljc42000@gmail.com>
2023-12-22 09:52:40 -0500

    Comment out

--- Algorithms/hypergraphDatabase.py
+++ Algorithms/hypergraphDatabase.py
@@ -200,7 +200,7 @@ kDf.to_csv('test.csv')
 
 # # Database connection information
 # DB_USERNAME = 'root'
-# DB_PASSWORD = 'Acd2023='
+# DB_PASSWORD = 'password'
 # DB_HOST = 'localhost'
 # DB_NAME = 'hypergraphs'
 
