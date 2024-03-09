632b0b9f4ae425ef12fdcb862eab6861e6a90d6f
Linnea <ljc42000@gmail.com>
2024-03-09 15:06:47 -0500

    Function Names

--- Algorithms/nestSetDatabase.py
+++ Algorithms/nestSetDatabase.py
@@ -5,9 +5,9 @@ import mysql.connector, copy, json
 import pandas as pd
 
 from getHypergraphs import getHypergraphs
-from nestSet import getSet
-from findNestPoints import *
-from sqlalchemy import create_engine
+from findNestSets import *
+from nestSetElimination import neo
+from sqlalchemy import create_engine, text
 
 # Create dataframe
 df = pd.DataFrame()
@@ -15,7 +15,7 @@ df = pd.DataFrame()
 
 # Database connection information
 DB_USERNAME = 'root'
-DB_PASSWORD = 'password'
+DB_PASSWORD = 'Acd2023='
 DB_HOST = 'localhost'
 DB_NAME = 'hypergraphs'
 
@@ -27,40 +27,67 @@ conn = mysql.connector.connect(
     database=DB_NAME
 )
 
-query = "SELECT hypergraph FROM hypergraphReduced WHERE numVertices = 8 AND sizeOfEdges = '[4, 4, 3]';"
+# query = "SELECT H.hypergraph FROM hypergraphReduced as H WHERE NOT EXISTS (SELECT 1 FROM nestSets as N WHERE H.hypergraph = N.hypergraph);"
+# query = "SELECT H.hypergraph FROM hypergraphReduced as H WHERE numVertices = 12;"
+query = "SELECT hypergraph FROM hypergraphReduced WHERE numVertices = 6"
 hypergraphList = getHypergraphs(DB_USERNAME, DB_PASSWORD,DB_HOST, DB_NAME, query)
 
-hypergraphs = []
-nestPoint = []
-twoNestSet = []
-threeNestSet = []
+def nestSets(hypergraphList):
+    hypergraphs = []
+    nestPoint = []
+    twoNestSet = []
+    threeNestSet = []
 
-for i in range(len(hypergraphList)):
-    hypergraphs.append(hypergraphList[i])
-    nestPoint.append(findNestPoint(copy.deepcopy(hypergraphList[i])))
-    twoNestSet.append(getSet(copy.deepcopy(hypergraphList[i]),2))
-    threeNestSet.append(getSet(copy.deepcopy(hypergraphList[i]),3))
+    for i in range(len(hypergraphList)):
+        hypergraphs.append(hypergraphList[i])
+        nestPoint.append(getSet(copy.deepcopy(hypergraphList[i]), 1))
+        twoNestSet.append(getSet(copy.deepcopy(hypergraphList[i]),2))
+        threeNestSet.append(getSet(copy.deepcopy(hypergraphList[i]),3))
 
-df['hypergraph'] = hypergraphs
-df['nestPoint'] = nestPoint
-df['twoNestSet'] = twoNestSet
-df['threeNestSet'] = threeNestSet
+    df['hypergraph'] = hypergraphs
+    df['nestPoint'] = nestPoint
+    df['twoNestSet'] = twoNestSet
+    df['threeNestSet'] = threeNestSet
 
-df['hypergraph'] = df['hypergraph'].apply(json.dumps)
-df['nestPoint'] = df['nestPoint'].apply(json.dumps)
-df['twoNestSet'] = df['twoNestSet'].apply(json.dumps)
-df['threeNestSet'] = df['threeNestSet'].apply(json.dumps)
+    df['hypergraph'] = df['hypergraph'].apply(json.dumps)
+    df['nestPoint'] = df['nestPoint'].apply(json.dumps)
+    df['twoNestSet'] = df['twoNestSet'].apply(json.dumps)
+    df['threeNestSet'] = df['threeNestSet'].apply(json.dumps)
 
-print(df)
+    # Create SQLAlchemy engine
+    engine = create_engine(f"mysql+mysqlconnector://{DB_USERNAME}:{DB_PASSWORD}@{DB_HOST}/{DB_NAME}")
+
+    df.to_sql('temp_table', engine, if_exists='replace')
+
+    sql = "UPDATE nestSets AS N INNER JOIN temp_table AS T ON N.hypergraph = T.hypergraph SET N.nestPoint = T.nestPoint, N.twoNestSet = T.twoNestSet, N.threeNestSet = T.threeNestSet;"
+
+    with engine.begin() as conn:
+        conn.execute(text(sql))
+
+    # Close the MySQL connection
+    conn.close()
+
+def nestSetElimination(hypergraphList):
+    hypergraphs = []
+    two = []
+
+    for i in range(len(hypergraphList)):
+        hypergraphs.append(hypergraphList[i])
+        two.append(neo(copy.deepcopy(hypergraphList[i]), 3))
+
+    df['hypergraph'] = hypergraphs
+    df['three'] = two
+
+    df['hypergraph'] = df['hypergraph'].apply(json.dumps)
+
+    # Create SQLAlchemy engine
+    engine = create_engine(f"mysql+mysqlconnector://{DB_USERNAME}:{DB_PASSWORD}@{DB_HOST}/{DB_NAME}")
+
+    df.to_sql('temp_table', engine, if_exists='replace')
+
+    # Close the MySQL connection
+    conn.close()   
+
+nestSetElimination(hypergraphList)
 
 
-df.to_csv('testNest.csv')
-
-# Create a SQLAlchemy engine
-engine = create_engine(f"mysql+mysqlconnector://{DB_USERNAME}:{DB_PASSWORD}@{DB_HOST}/{DB_NAME}")
-
-# Insert DataFrame into hypergraph table
-df.to_sql('nestSets', con=engine, if_exists='append', index=False)
-
-# Close the MySQL connection
-conn.close()
